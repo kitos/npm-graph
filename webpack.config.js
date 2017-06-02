3d3d1ca017914e63058ee19bb184c8335708a105
Nikita Kirsanov <nikita_kirsanov@epam.com>
2017-06-02 16:12:38 +0300

    minify js bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
 const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
+
     entry: './src/index.js',
+
     output: {
         path: path.resolve('./public'),
         filename: 'bundle.js',
     },
+
+    module: {
+
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ],
+    },
+
+    plugins:[
+        new webpack.optimize.UglifyJsPlugin()
+    ]
 }
